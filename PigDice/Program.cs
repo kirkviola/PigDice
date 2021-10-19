340b53a2b528ba2e8353cc1dd4cb5949b409c601
Matthew Kirkendall <kirk.viola@gmail.com>
2021-10-19 15:59:45 -0400

    Add project files.

---
+++ PigDice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool over = false;
+
+            double score = 0;
+            double currentScore = 0;
+
+            for(var i = 0; i <= 2000000000; i++)
+            {
+                currentScore = 0;
+                while (!over)
+                {
+                    var roll = RandomNumberGenerator.GetInt32(1, 7);
+                    if (roll == 1)
+                        break;
+                    else
+                        currentScore += roll;
+                }
+                if (currentScore > score)
+                {
+                    score = currentScore;
+                    Console.WriteLine(score);
+                }
+            }
+
+            Console.WriteLine(score);
+        }
+    }
+}
